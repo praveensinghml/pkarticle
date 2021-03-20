4ef72360641b9ecb2039d17bf09bf08545cf55bf
LEGAL\singhp7 <praveen.singh@lexisnexis.com>
2021-03-20 10:31:58 +0530

    tweaked app for heroku

--- blog/settings.py
+++ blog/settings.py
@@ -1,5 +1,8 @@
 import os
 
+####For heroku config
+import dj_database_url
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -48,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ####For heroku config
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'blog.urls'
@@ -147,6 +152,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+####For heroku config
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Tinymce
 
 TINYMCE_DEFAULT_CONFIG = {
@@ -188,3 +195,6 @@ AUTHENTICATION_BACKENDS = (
 )
 
 SITE_ID = 1
+
+####For heroku config
+django_heroku.settings(locals())
